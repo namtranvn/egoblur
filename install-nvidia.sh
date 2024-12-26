dbca19d0517f73914f6044eea6d86874a6039ac2
Nam Tran <hoainamtran204@gmail.com>
2024-12-26 13:00:00 +0900

    change centos

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,25 +1,25 @@
 # Add NVIDIA package repositories
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo rpm --import - \
+    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+
+# Install required packages
+sudo yum install -y epel-release
+sudo yum clean expire-cache
 
 # Install nvidia-docker2
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2
+sudo yum install -y nvidia-docker2
 
-#
-sudo apt-get update
-sudo apt-get install -y nvidia-cuda-toolkit
+# Install NVIDIA CUDA toolkit
+sudo yum install -y nvidia-cuda-toolkit
 
-#
-nvidia-smi 
+# Verify NVIDIA drivers are working
+nvidia-smi
 
 # Install NVIDIA Container Toolkit
-sudo apt-get update
-sudo apt-get install -y nvidia-container-toolkit
+sudo yum install -y nvidia-container-toolkit
 
 # Configure Docker daemon
 sudo nvidia-ctk runtime configure --runtime=docker
 
 # Restart Docker daemon
-sudo systemctl restart docker
